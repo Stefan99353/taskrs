661d19c7b7be87c76d21569291237fcc0d2852bc
Stefan Rupertsberger <s.rupertsberger@gmail.com>
2022-01-15 15:57:10 +0100

    Added model files for permissions

---
+++ taskrs-db/src/models/permission/dtos.rs
@@ -0,0 +1,96 @@
+use chrono::NaiveDateTime;
+use sea_orm::ActiveValue;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Permission {
+    pub id: i32,
+    pub name: String,
+    pub group: String,
+    pub description: Option<String>,
+    pub inserted_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+}
+
+impl From<crate::models::permission::Model> for Permission {
+    fn from(model: crate::models::permission::Model) -> Self {
+        Self {
+            id: model.id,
+            name: model.name,
+            group: model.group,
+            description: None,
+            inserted_at: model.inserted_at,
+            updated_at: model.updated_at,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PermissionCreate {
+    pub name: String,
+    pub group: String,
+    pub description: Option<String>,
+    pub inserted_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+}
+
+impl From<PermissionCreate> for crate::models::permission::ActiveModel {
+    fn from(dto: PermissionCreate) -> Self {
+        let mut active_model = Self {
+            name: ActiveValue::Set(dto.name),
+            group: Default::default(),
+            description: Default::default(),
+            ..Default::default()
+        };
+
+        if let Some(inserted_at) = dto.inserted_at {
+            active_model.inserted_at = ActiveValue::Set(Some(inserted_at));
+        }
+        if let Some(updated_at) = dto.updated_at {
+            active_model.updated_at = ActiveValue::Set(Some(updated_at));
+        }
+
+        active_model
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PermissionUpdate {
+    pub id: i32,
+    pub name: Option<String>,
+    pub group: Option<String>,
+    pub description: Option<Option<String>>,
+    pub inserted_at: Option<Option<NaiveDateTime>>,
+    pub updated_at: Option<Option<NaiveDateTime>>,
+}
+
+impl From<PermissionUpdate> for crate::models::permission::ActiveModel {
+    fn from(dto: PermissionUpdate) -> Self {
+        let mut active_model = Self {
+            id: ActiveValue::Set(dto.id),
+            ..Default::default()
+        };
+
+        if let Some(name) = dto.name {
+            active_model.name = ActiveValue::Set(name);
+        }
+        if let Some(group) = dto.group {
+            active_model.group = ActiveValue::Set(group);
+        }
+        if let Some(description) = dto.description {
+            active_model.description = ActiveValue::Set(description);
+        }
+
+        if let Some(inserted_at) = dto.inserted_at {
+            active_model.inserted_at = ActiveValue::Set(inserted_at);
+        }
+        if let Some(updated_at) = dto.updated_at {
+            active_model.updated_at = ActiveValue::Set(updated_at);
+        }
+
+        active_model
+    }
+}
---
+++ taskrs-db/src/models/permission/mod.rs
@@ -0,0 +1,83 @@
+pub mod dtos;
+
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, Default, DeriveModel, DeriveActiveModel)]
+pub struct Model {
+    pub id: i32,
+    pub name: String,
+    pub group: String,
+    pub description: Option<String>,
+    pub inserted_at: Option<DateTime>,
+    pub updated_at: Option<DateTime>,
+}
+
+#[derive(Copy, Clone, Default, Debug, DeriveEntity)]
+pub struct Entity;
+
+impl EntityName for Entity {
+    fn table_name(&self) -> &str {
+        "permissions"
+    }
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
+pub enum Column {
+    Id,
+    Name,
+    Group,
+    Description,
+    InsertedAt,
+    UpdatedAt,
+}
+
+impl ColumnTrait for Column {
+    type EntityName = Entity;
+
+    fn def(&self) -> ColumnDef {
+        match self {
+            Self::Id => ColumnType::Integer.def(),
+            Self::Name => ColumnType::String(Some(256)).def(),
+            Self::Group => ColumnType::String(Some(256)).def(),
+            Self::Description => ColumnType::String(None).def().nullable(),
+            Self::InsertedAt => ColumnType::DateTime.def(),
+            Self::UpdatedAt => ColumnType::DateTime.def(),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DerivePrimaryKey)]
+pub enum PrimaryKey {
+    Id,
+}
+
+impl PrimaryKeyTrait for PrimaryKey {
+    type ValueType = i32;
+
+    fn auto_increment() -> bool {
+        true
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {
+    #[cfg(feature = "db-timestamps")]
+    fn before_save(mut self, insert: bool) -> Result<Self, DbErr> {
+        let timestamp = chrono::Utc::now().naive_utc();
+
+        // Inserted timestamp
+        if let (&sea_orm::ActiveValue::NotSet, true) = (&self.inserted_at, insert) {
+            self.inserted_at = sea_orm::ActiveValue::Set(Some(timestamp));
+        }
+
+        // Updated timestamp
+        if let sea_orm::ActiveValue::NotSet = self.updated_at {
+            self.updated_at = sea_orm::ActiveValue::Set(Some(timestamp));
+        }
+
+        Ok(self)
+    }
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+}
