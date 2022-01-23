f98d3b819eec72e7fdff295d150b40c099c297f5
Stefan Rupertsberger <s.rupertsberger@gmail.com>
2022-01-23 21:00:25 +0100

    Implement debug logs

--- taskrs-db/src/actions/permissions.rs
+++ taskrs-db/src/actions/permissions.rs
@@ -6,6 +6,16 @@ use sea_orm::sea_query::SimpleExpr;
 use sea_orm::{Condition, Order, QueryOrder};
 
 /// Gets all permissions from database
+#[instrument(
+    name = "get_all_permissions"
+    level = "debug",
+    skip_all,
+    err,
+    fields (
+        condition = tracing::field::debug(&condition),
+        order = tracing::field::debug(&order),
+    )
+)]
 pub async fn get_all(
     condition: Option<Condition>,
     order: Option<Vec<(Order, SimpleExpr)>>,
@@ -14,15 +24,18 @@ pub async fn get_all(
     let mut query = permission::Entity::find();
 
     if let Some(condition) = condition {
+        trace!("Apply condition to query");
         query = query.filter(condition);
     }
 
     if let Some(order) = order {
+        trace!("Apply order to query");
         for (ord, col) in order {
             query = query.order_by(col, ord);
         }
     }
 
+    debug!("Executing query");
     query
         .all(db)
         .await
@@ -30,6 +43,18 @@ pub async fn get_all(
 }
 
 /// Gets all permissions from database in a paginated form
+#[instrument(
+    name = "get_paginated_permissions"
+    level = "debug",
+    skip_all,
+    err,
+    fields (
+        page = page,
+        limit = limit,
+        condition = tracing::field::debug(&condition),
+        order = tracing::field::debug(&order),
+    )
+)]
 pub async fn get_paginated(
     page: usize,
     limit: usize,
@@ -40,15 +65,18 @@ pub async fn get_paginated(
     let mut query = permission::Entity::find();
 
     if let Some(condition) = condition {
+        trace!("Apply condition to query");
         query = query.filter(condition);
     }
 
     if let Some(order) = order {
+        trace!("Apply order to query");
         for (ord, col) in order {
             query = query.order_by(col, ord);
         }
     }
 
+    debug!("Executing query and getting count of items");
     let paginator = query.paginate(db, limit);
     let (models, count) = try_join!(paginator.fetch_page(page), paginator.num_items())?;
 
@@ -56,6 +84,16 @@ pub async fn get_paginated(
 }
 
 /// Gets a single permission from the database using an ID and/or a condition
+#[instrument(
+    name = "get_permission"
+    level = "debug",
+    skip_all,
+    err,
+    fields (
+        id = tracing::field::debug(id),
+        condition = tracing::field::debug(&condition),
+    )
+)]
 pub async fn get(
     id: Option<i32>,
     condition: Option<Condition>,
@@ -68,39 +106,71 @@ pub async fn get(
     };
 
     if let Some(condition) = condition {
+        trace!("Apply condition to query");
         query = query.filter(condition);
     }
 
+    debug!("Executing query");
     query.one(db).await.map(|opt| opt.map(|model| model.into()))
 }
 
 /// Creates a new permission
+#[instrument(
+    name = "create_permission"
+    level = "debug",
+    skip_all,
+    err,
+)]
 pub async fn create(permission: PermissionCreate, db: &DbConn) -> Result<Permission, DbErr> {
     let active_model: permission::ActiveModel = permission.into();
+    debug!("Inserting new permission");
     active_model.insert(db).await.map(|model| model.into())
 }
 
 /// Updates a permission
+#[instrument(
+    name = "update_permission"
+    level = "debug",
+    skip_all,
+    err,
+    fields (
+        id = permission.id
+    )
+)]
 pub async fn update(permission: PermissionUpdate, db: &DbConn) -> Result<Permission, DbErr> {
     let active_model: permission::ActiveModel = permission.into();
+    debug!("Updating permission");
     active_model.insert(db).await.map(|model| model.into())
 }
 
 /// Deletes a permission
+#[instrument(
+    name = "delete_permissions"
+    level = "debug",
+    skip_all,
+    err,
+    fields (
+        id = tracing::field::debug(id),
+        condition = tracing::field::debug(&condition),
+    )
+)]
 pub async fn delete(
     id: Option<i32>,
     condition: Option<Condition>,
     db: &DbConn,
 ) -> Result<u64, DbErr> {
-    let mut query = if let Some(id) = id {
-        permission::Entity::delete_many().filter(permission::Column::Id.eq(id))
-    } else {
-        permission::Entity::delete_many()
-    };
+    let mut query = permission::Entity::delete_many();
+
+    if let Some(id) = id {
+        trace!("Filter for ID");
+        query = query.filter(permission::Column::Id.eq(id))
+    }
 
     if let Some(condition) = condition {
+        trace!("Apply condition to query");
         query = query.filter(condition);
     }
 
+    debug!("Executing query");
     query.exec(db).await.map(|res| res.rows_affected)
 }
