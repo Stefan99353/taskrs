8eafcc27369129e6112d3e573c35d3b8ea7dcfb8
Stefan Rupertsberger <s.rupertsberger@gmail.com>
2022-02-01 21:11:10 +0100

    Ability to extract AccessTokenData from request
    Used for validating access tokens; returns error if no or invalid token

--- taskrs-api/src/dtos/token_data.rs
+++ taskrs-api/src/dtos/token_data.rs
@@ -1,3 +1,10 @@
+use crate::api::error::ApiError;
+use crate::application::ApplicationState;
+use axum::async_trait;
+use axum::extract::{Extension, FromRequest, RequestParts, TypedHeader};
+use axum::headers::authorization::Bearer;
+use axum::headers::Authorization;
+use axum::response::{IntoResponse, Response};
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
 
@@ -16,6 +23,39 @@ impl AccessTokenData {
             exp: now + exp as i64,
             user,
         }
+    }
+}
+
+#[async_trait]
+impl<B> FromRequest<B> for AccessTokenData
+where
+    B: Send,
+{
+    type Rejection = Response;
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        // Extract state
+        let Extension(state): Extension<ApplicationState> = Extension::from_request(req)
+            .await
+            .map_err(|err| err.into_response())?;
+
+        // Extract the token from authorization header
+        let TypedHeader(Authorization(bearer)) =
+            TypedHeader::<Authorization<Bearer>>::from_request(req)
+                .await
+                .map_err(|_| ApiError::InvalidAccessToken.into_response())?;
+
+        // Decode the data
+        let token_data = jsonwebtoken::decode::<Self>(
+            bearer.token(),
+            &jsonwebtoken::DecodingKey::from_secret(
+                state.config.authentication.access_token_secret.as_bytes(),
+            ),
+            &jsonwebtoken::Validation::default(),
+        )
+        .map_err(|_| ApiError::InvalidAccessToken.into_response())?;
+
+        Ok(token_data.claims)
     }
 }
 
